ca234173ae37bdba2e71ff77b2e184367049c71d
dridi14 <dridi.mohamedamine@hxperience.com>
2023-07-21 05:38:06 +0200

    dd

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import dj_database_url
 import datetime
 from pathlib import Path
 import os
