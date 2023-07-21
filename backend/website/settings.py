acbdbe6d2a46418a339040fee404d4a4965e0064
dridi14 <dridi.mohamedamine@hxperience.com>
2023-07-21 04:37:43 +0200

    ss

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import dj_database_url
 import datetime
 from pathlib import Path
+import os
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
