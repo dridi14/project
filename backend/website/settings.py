baf3476d64279c73ed72eda9e60bfe2c91776f2c
dridi14 <dridi.mohamedamine@hxperience.com>
2023-07-21 05:07:07 +0200

    s

--- backend/website/settings.py
+++ backend/website/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mydatabase',
         'USER': 'mydatabaseuser',
         'PASSWORD': 'mypassword',
